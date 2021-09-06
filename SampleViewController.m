854ebaa7152e2ff54e0f8d25eefa839ac1908743
Abhishek Singh <Abhishek_110388@yahoo.com>
2021-09-06 11:00:14 +0530

    Add react

---
+++ SampleViewController.m
@@ -0,0 +1,32 @@
+//
+//  SampleViewController.m
+//  Native_SDK
+//
+//  Created by Abhishek Singh on 06/09/21.
+//
+
+#import "SampleViewController.h"
+#import "RCTRootView.h"
+
+@interface SampleViewController ()
+
+@end
+
+@implementation SampleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
