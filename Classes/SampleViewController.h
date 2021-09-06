3dd0b9022eacc57b7f07cccf3ed77723bbbaa7db
Abhishek Singh <Abhishek_110388@yahoo.com>
2021-09-06 11:06:07 +0530

    file added

---
+++ Classes/SampleViewController.h
@@ -0,0 +1,16 @@
+//
+//  SampleViewController.h
+//  Native_SDK
+//
+//  Created by Abhishek Singh on 06/09/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SampleViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/SampleViewController.m
@@ -0,0 +1,31 @@
+//
+//  SampleViewController.m
+//  Native_SDK
+//
+//  Created by Abhishek Singh on 06/09/21.
+//
+
+#import "SampleViewController.h"
+
+@interface SampleViewController ()
+
+@end
+
+@implementation SampleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
